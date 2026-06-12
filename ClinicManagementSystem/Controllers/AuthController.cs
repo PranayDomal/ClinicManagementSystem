046963d73e1964be0d866f5f5cd42e14a411455d
Pranay Domal <domalpranay@gmail.com>
2026-06-12 11:56:10 +0530

    ASP.NET Core Project

---
+++ ClinicManagementSystem/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicManagementSystem.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
